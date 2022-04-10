30dc0a3c19b4c6d1319f6895d3b3b525ccbcb1ad
Carsten Bauer <carsten.bauer@uni-paderborn.de>
2022-04-10 07:37:34 +0200

    update n2login3 folder

--- n2login3/run_bench.sh
+++ n2login3/run_bench.sh
@@ -1,14 +1,11 @@
-# srun -u -n 1 -c 128 --cpu_bind=sockets
-# srun -u -n 1 -c 128 --cpu_bind=sockets
-
 rm -f results.csv
 
-OMP_NUM_THREADS=128 OMP_PLACES=threads OMP_PROC_BIND=close julia-beta --project bench.jl OpenBLAS highAI
-OMP_NUM_THREADS=64 OMP_PLACES=cores OMP_PROC_BIND=spread julia-beta --project bench.jl OpenBLAS highMB
-OMP_NUM_THREADS=128 OMP_PLACES=threads OMP_PROC_BIND=close julia-beta --project bench.jl MKL highAI
-OMP_NUM_THREADS=64 OMP_PLACES=cores OMP_PROC_BIND=spread julia-beta --project bench.jl MKL highMB
+OMP_NUM_THREADS=128 OMP_PLACES=threads OMP_PROC_BIND=close julia-beta --project=.. ../bench.jl OpenBLAS highAI
+OMP_NUM_THREADS=64 OMP_PLACES=cores OMP_PROC_BIND=spread julia-beta --project=.. ../bench.jl OpenBLAS highMB
+OMP_NUM_THREADS=128 OMP_PLACES=threads OMP_PROC_BIND=close julia-beta --project=.. ../bench.jl MKL highAI
+OMP_NUM_THREADS=64 OMP_PLACES=cores OMP_PROC_BIND=spread julia-beta --project=.. ../bench.jl MKL highMB
 export LD_PRELOAD=/scratch/pc2-mitarbeiter/bauerc/devel/julia-mkl-amd/mkl_workaround/libfakeintel.so
-OMP_NUM_THREADS=128 OMP_PLACES=threads OMP_PROC_BIND=close julia-beta --project bench.jl MKL highAI
-OMP_NUM_THREADS=64 OMP_PLACES=cores OMP_PROC_BIND=spread julia-beta --project bench.jl MKL highMB
+OMP_NUM_THREADS=128 OMP_PLACES=threads OMP_PROC_BIND=close julia-beta --project=.. ../bench.jl MKL highAI
+OMP_NUM_THREADS=64 OMP_PLACES=cores OMP_PROC_BIND=spread julia-beta --project=.. ../bench.jl MKL highMB
 
-julia-beta --project plot_results.jl
+julia-beta --project ../plot_results.jl
